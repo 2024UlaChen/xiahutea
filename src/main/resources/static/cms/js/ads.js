8bcde53682817ce66ea5ab6e0233e97d6dfead37
Ray <a55663613@gmail.com>
2024-08-19 12:36:28 +0800

    修改按鈕樣式及新增日期選擇限制JS

--- src/main/resources/static/cms/js/ads.js
+++ src/main/resources/static/cms/js/ads.js
@@ -2,8 +2,18 @@ document.addEventListener("DOMContentLoaded", function (){
     //抓取標籤
     let title_el = document.getElementById("title");
     let description_el = document.getElementById("description");
-
-
+    let start_date_el = document.getElementById("start_date")
+    let end_date_el = document.getElementById("end_date")
+    //事件綁定
+    //日期選擇限制
+    start_date_el.addEventListener('change', function () {
+        var startDate = this.value;
+        end_date_el.setAttribute('min', startDate);
+    });
+    end_date_el.addEventListener('change', function () {
+        var endDate = this.value;
+        start_date_el.setAttribute('max', endDate);
+    });
     //placeholder處理
     title_el.addEventListener("focus", function () {
         // 清空 placeholder
@@ -25,7 +35,4 @@ document.addEventListener("DOMContentLoaded", function (){
             description_el.placeholder = "可輸入255字元單位";
         }
     });
-
-
-
 })
