b2afce0c08da00e2d5cc09b9bcf1ff11eabd42d1
Sherry <hsuan.wu9211@gmail.com>
2024-08-22 17:20:23 +0800

    Add DisputeOrder Dao

--- src/main/java/idv/tia201/g2/web/order/dao/DisputeDao.java
+++ src/main/java/idv/tia201/g2/web/order/dao/DisputeDao.java
@@ -1,4 +1,15 @@
 package idv.tia201.g2.web.order.dao;
 
-public class DisputeDao {
+import idv.tia201.g2.web.order.vo.DisputeOrder;
+import idv.tia201.g2.web.order.vo.Orders;
+
+import java.util.List;
+
+public interface DisputeDao {
+    int insert(DisputeOrder disputeOrder);
+    int update(DisputeOrder disputeOrder);
+
+    Orders selectByDisputeOrderId(int disputeOrderId);
+    List<DisputeOrder> selectAll();
+
 }
--- src/main/java/idv/tia201/g2/web/order/dao/impl/DisputeDaoImpl.java
+++ src/main/java/idv/tia201/g2/web/order/dao/impl/DisputeDaoImpl.java
@@ -1,4 +1,29 @@
 package idv.tia201.g2.web.order.dao.impl;
 
-public class DisputeDaoImpl {
+import idv.tia201.g2.web.order.dao.DisputeDao;
+import idv.tia201.g2.web.order.vo.DisputeOrder;
+import idv.tia201.g2.web.order.vo.Orders;
+
+import java.util.List;
+
+public class DisputeDaoImpl implements DisputeDao {
+    @Override
+    public int insert(DisputeOrder disputeOrder) {
+        return 0;
+    }
+
+    @Override
+    public int update(DisputeOrder disputeOrder) {
+        return 0;
+    }
+
+    @Override
+    public Orders selectByDisputeOrderId(int disputeOrderId) {
+        return null;
+    }
+
+    @Override
+    public List<DisputeOrder> selectAll() {
+        return List.of();
+    }
 }
