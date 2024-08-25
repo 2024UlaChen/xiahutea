54139a9b2f3924d01d82a37f99741ba0a5aa7ecc
Kun <s58803194@gmail.com>
2024-08-25 21:42:28 +0800

    商家商業邏輯

--- src/main/java/idv/tia201/g2/web/store/service/StoreService.java
+++ src/main/java/idv/tia201/g2/web/store/service/StoreService.java
@@ -1,4 +1,24 @@
 package idv.tia201.g2.web.store.service;
 
+import idv.tia201.g2.web.store.vo.Store;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface StoreService {
+    //列出全部
+    List<Store> findAll();
+    List<Store> findAll(Pageable pageable);
+    //搜尋店家 只要特定字
+    List<Store> findStoreByName(String name);
+
+    //用於查修 準確指定
+    Store findStoreById(Integer id);
+
+    List<Store> findStoreByAddress(String address);
+    Store saveOrUpdateStore(Store store);
+
+
+
+    //是否設置移除店家??
 }
