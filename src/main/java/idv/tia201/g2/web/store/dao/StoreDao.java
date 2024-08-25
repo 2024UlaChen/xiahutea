53fa684842d4c789b9451a3cf55257f4ecf8c430
Kun <s58803194@gmail.com>
2024-08-25 19:29:06 +0800

    StoreDao 加入 模糊查詢區域與店名

--- src/main/java/idv/tia201/g2/web/store/dao/StoreDao.java
+++ src/main/java/idv/tia201/g2/web/store/dao/StoreDao.java
@@ -9,8 +9,24 @@ import java.util.List;
 import java.util.Optional;
 
 public interface StoreDao extends JpaRepository<Store, Integer> {
-    List<Store> findByStoreName(String storeName);
-    @Query("FROM Store WHERE storeAddress LIKE %:area")
-    List<Store> findByStoreAddress(@Param("area") String storeAddress);
+
+    //List<Store> findByStoreName(String storeName);
+
+    //根據店名進行模糊搜尋
+//    @Query("FROM Store s WHERE s.storeName LIKE %:name%")
+//    List<Store> findByStoreNameLike(@Param("name") String storeName);
+
+    //根據區域選擇商家 使用JPQL語法 查詢的是Entity
+//    @Query("FROM Store WHERE storeAddress LIKE %:area%")
+//    List<Store> findByStoreAddress(@Param("area") String storeAddress);
+//    @Query("FROM Store WHERE storeName LIKE %:name% OR storeAddress LIKE %:area ")
+//    List<Store> findByStoreNameOrStoreAddress(@Param("name") String name, @Param("area")String address);
+// 以上都有更簡潔的生成查詢
+    //模糊查詢店名  並且大小寫不敏感
+    List<Store> findByStoreNameContainingIgnoreCase(String name);
+    //區域模糊查詢
+    List<Store> findByStoreAddressContaining(String address);
+    //根據區域 和店名 模糊查詢
+    List<Store> findByStoreNameContainingOrStoreAddressContaining(String name, String address);
 
 }
