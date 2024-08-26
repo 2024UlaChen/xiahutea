351364a39201da01864c469c25b96d985497cec4
Harper <wendy8564@gmail.com>
2024-08-26 23:04:16 +0800

    建立 AdminDao 檔案

--- src/main/java/idv/tia201/g2/web/user/dao/AdminDao.java
+++ src/main/java/idv/tia201/g2/web/user/dao/AdminDao.java
@@ -1,8 +1,11 @@
 package idv.tia201.g2.web.user.dao;
 
+import idv.tia201.g2.web.user.vo.Administrators;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@Repository
+
 public interface AdminDao {
+    public Administrators selectForLogin(@RequestParam String username, String password);
 
 }
