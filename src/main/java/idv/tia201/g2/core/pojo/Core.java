b233c829f46068f0d32778647b3b65a09b6fa72e
Harper <wendy8564@gmail.com>
2024-08-26 22:59:23 +0800

    增加 Core 的 @Data & @Serial

--- src/main/java/idv/tia201/g2/core/pojo/Core.java
+++ src/main/java/idv/tia201/g2/core/pojo/Core.java
@@ -1,8 +1,12 @@
 package idv.tia201.g2.core.pojo;
 
-import java.io.Serializable;
+import lombok.Data;
 
+import java.io.Serial;
+import java.io.Serializable;
+@Data
 public class Core implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1457755989409740329L;
     private boolean successful;
     private String message;
