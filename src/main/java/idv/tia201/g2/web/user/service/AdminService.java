995a6cfe769283a1184cef60679b2b70f0a869e8
Harper <wendy8564@gmail.com>
2024-08-26 23:07:11 +0800

    將 UserLoginService 改成 AdminService

---
+++ src/main/java/idv/tia201/g2/web/user/service/AdminService.java
@@ -0,0 +1,8 @@
+package idv.tia201.g2.web.user.service;
+
+
+import idv.tia201.g2.web.user.vo.Administrators;
+
+public interface AdminService {
+    public Administrators login(Administrators admin);
+}
