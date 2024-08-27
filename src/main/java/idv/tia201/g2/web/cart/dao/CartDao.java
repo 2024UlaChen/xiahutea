14565ddd2d2e011683f6d24c2c16afae8db844de
Ray <a55663613@gmail.com>
2024-08-27 17:50:25 +0800

    初步設計DAO層及SERVICE層介面及方法，Product相關DAO尚未建立，仍先於service中引用 問題點:考量是否需要DAO中

--- src/main/java/idv/tia201/g2/web/cart/dao/CartDao.java
+++ src/main/java/idv/tia201/g2/web/cart/dao/CartDao.java
@@ -1,7 +1,9 @@
 package idv.tia201.g2.web.cart.dao;
 
+import idv.tia201.g2.web.product.vo;
 import idv.tia201.g2.web.member.vo.Cart;
 
 public interface CartDao {
-    Cart selectByProductId(int id);
+    Cart selectByProductId(Integer id);
+
 }
--- src/main/java/idv/tia201/g2/web/cart/service/CartService.java
+++ src/main/java/idv/tia201/g2/web/cart/service/CartService.java
@@ -1,4 +1,9 @@
 package idv.tia201.g2.web.cart.service;
 
+import idv.tia201.g2.web.member.vo.Cart;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public interface CartService {
+
+    Cart getProductDetail(Integer productId);
 }
