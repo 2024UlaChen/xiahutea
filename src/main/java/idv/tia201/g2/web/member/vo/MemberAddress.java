e5ca158b2edb5b621ebb150b0ddd5a689b9ac725
May.Chen <may.chen82612090@gmail.com>
2024-08-27 12:51:51 +0800

    add annotation

--- src/main/java/idv/tia201/g2/web/member/vo/MemberAddress.java
+++ src/main/java/idv/tia201/g2/web/member/vo/MemberAddress.java
@@ -1,4 +1,20 @@
 package idv.tia201.g2.web.member.vo;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "customer_address")
 public class MemberAddress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_address_id")
+    private Integer customerAddressId;
+
+    @Column(name = "customer_id")
+    private Integer customerId;
+
+    @Column(name = "customer_address")
+    private String customerAddress;
+
 }
