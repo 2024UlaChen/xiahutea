926b62ce9d41d594f07b33d98f391bbaf3cc1dd3
Ray <a55663613@gmail.com>
2024-08-27 18:59:46 +0800

    修改正確import位置

--- src/main/java/idv/tia201/g2/web/cart/dao/CartDao.java
+++ src/main/java/idv/tia201/g2/web/cart/dao/CartDao.java
@@ -1,6 +1,6 @@
 package idv.tia201.g2.web.cart.dao;
 
-import idv.tia201.g2.web.product.vo;
+import idv.tia201.g2.web.product.vo.Product;
 import idv.tia201.g2.web.member.vo.Cart;
 
 public interface CartDao {
