04df1a72d74469e02f6795c37c3d1a1fdacaed8d
May.Chen <may.chen82612090@gmail.com>
2024-09-03 13:58:11 +0800

    update login controller

--- src/main/java/idv/tia201/g2/web/member/controller/LoginController.java
+++ src/main/java/idv/tia201/g2/web/member/controller/LoginController.java
@@ -1,16 +1,18 @@
 package idv.tia201.g2.web.member.controller;
 
+import idv.tia201.g2.web.member.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/login")
+@Controller
 public class LoginController extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = -2956734137070898975L;
+    @Autowired
+    private MemberService memberService;
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-
-    }
 }
