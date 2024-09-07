c9025eb3799d7f95e4e28eb315f224253baf05f4
May <may.chen82612090@gmail.com>
2024-09-07 18:16:02 +0800

    update member carrier

--- src/main/resources/static/js/member.js
+++ src/main/resources/static/js/member.js
@@ -6,25 +6,34 @@ document.addEventListener("DOMContentLoaded", function () {
         if (nowLocation.indexOf(el.getAttribute("href")) !== -1) {
             el.closest("li").classList.add("member-list-active");
             el.style.color = "#73B4BA";
-        }else{
-            el.addEventListener("mouseover",function(){
+        } else {
+            el.addEventListener("mouseover", function () {
                 el.style.color = "#FAC792";
-                el.closest("li").style.backgroundColor="#F8F9FA";
-                el.closest("li").style.borderLeft="solid 5px #FAC792";
+                el.closest("li").style.backgroundColor = "#F8F9FA";
+                el.closest("li").style.borderLeft = "solid 5px #FAC792";
             });
-            el.addEventListener("mouseout",function () {
+            el.addEventListener("mouseout", function () {
                 el.style.color = "initial";
-                el.closest("li").style.backgroundColor="white";
-                el.closest("li").style.borderLeft="solid 5px white";
+                el.closest("li").style.backgroundColor = "white";
+                el.closest("li").style.borderLeft = "solid 5px white";
             })
         }
     })
 
     // 載具
-    const myCarrierText = document.querySelector("#memberCarrierText");
+    const memberCarrierBarcode = document.querySelector("#memberCarrierBarcode");
+    const memberCarrierText = document.querySelector("#memberCarrierText");
+    let memberCarrierTextValue = "/XXX-AAA";
+    memberCarrierText.innerText = memberCarrierTextValue;
+
     function getCarrier() {
-        JsBarcode(myCarrierText, "123456");
+        JsBarcode(memberCarrierBarcode, memberCarrierTextValue, {
+            displayValue: false
+        });
     };
     getCarrier();
 });
+
+const editMemberCarrier = document.querySelector(".memberCarrierEdit");
+
 
