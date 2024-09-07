c1b40902f74d421cd1058e6b55daf084b3cb44a1
Harper <wendy8564@gmail.com>
2024-09-07 13:28:39 +0800

    更新 載入方式

--- src/main/resources/static/cms/js/storegeneral.js
+++ src/main/resources/static/cms/js/storegeneral.js
@@ -1,3 +1,19 @@
-$(".storeheader").load("storeheader.html");
-$(".storeaside").load("storeaside.html");
-$(".storefooter").load("storefooter.html");
+myLoad(".storeheader","storeheader.html")
+myLoad(".storeaside","storeaside.html")
+myLoad(".storefooter","storefooter.html")
+
+function myLoad(target,url){
+    $.ajax({
+        type: "GET",
+        url: url,
+        async:false,
+        dataType: "text",
+        success: function (response) {
+            $(target).html(response);
+        },
+        error: function (thrownError) {
+            console.log(thrownError);
+        }
+    });
+}
+
