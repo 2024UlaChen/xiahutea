23847322ead1be45854926245e72e24a633ed061
May <may.chen82612090@gmail.com>
2024-09-07 21:47:29 +0800

    update carrier html / css / js

--- src/main/resources/static/js/member.js
+++ src/main/resources/static/js/member.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     })
 
-    // 載具
+    // memberCarrier
     const memberCarrierBarcode = document.querySelector("#memberCarrierBarcode");
     const memberCarrierText = document.querySelector("#memberCarrierText");
     let memberCarrierTextValue = "/XXX-AAA";
@@ -32,8 +32,60 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     };
     getCarrier();
+
+
+    const carrierSaveBtn = document.querySelector("#carrierSaving");
+    const carrierDelBtn = document.querySelector("#carrierDeleting");
+    const carrierEditingCloseBtn = document.querySelector(".close");
+
+    carrierSaveBtn.addEventListener("click", function () {
+        carrierEditingCloseBtn.click();
+        console.log("click");
+        Swal.fire({
+            position:"top",
+            icon: "success",
+            title: "Your work has been saved",
+            showConfirmButton: false,
+            timer: 1500
+        });
+    })
+
+    // carrierDelBtn.addEventListener("click", function () {
+    //
+    //     const swalWithBootstrapButtons = Swal.mixin({
+    //         customClass: {
+    //             confirmButton: "btn btn-success",
+    //             cancelButton: "btn btn-danger"
+    //         },
+    //         buttonsStyling: false
+    //     });
+    //     swalWithBootstrapButtons.fire({
+    //         title: "刪除載具?",
+    //         icon: "warning",
+    //         showCancelButton: true,
+    //         confirmButtonText: "刪除",
+    //         cancelButtonText: "取消",
+    //         reverseButtons: true
+    //     }).then((result) => {
+    //         if (result.isConfirmed) {
+    //             swalWithBootstrapButtons.fire({
+    //                 text: "已刪除載具",
+    //                 icon: "success"
+    //             });
+    //         } else if (
+    //             /* Read more about handling dismissals below */
+    //             result.dismiss === Swal.DismissReason.cancel
+    //         ) {
+    //             swalWithBootstrapButtons.fire({
+    //                 title: "Cancelled",
+    //                 timer: 1000
+    //             });
+    //         }
+    //     });
+    //
+    // });
+
 });
 
-const editMemberCarrier = document.querySelector(".memberCarrierEdit");
 
 
