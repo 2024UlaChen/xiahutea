7b1faa6dc74a7c1a3f4122ee524cd514149c9d68
Harper <wendy8564@gmail.com>
2024-09-11 21:35:06 +0800

    密碼加密工具

---
+++ src/main/java/idv/tia201/g2/core/util/EncrypSHA.java
@@ -0,0 +1,28 @@
+package idv.tia201.g2.core.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class EncrypSHA {
+    public static String SHAEncrypt(final String content) {
+        try {
+            MessageDigest sha = MessageDigest.getInstance("SHA");
+            byte[] sha_byte = sha.digest(content.getBytes());
+            StringBuffer hexValue = new StringBuffer();
+            for (byte b : sha_byte) {
+                //将其中的每个字节转成十六进制字符串：byte类型的数据最高位是符号位，通过和0xff进行与操作，转换为int类型的正整数。
+                String toHexString = Integer.toHexString(b & 0xff);
+                hexValue.append(toHexString.length() == 1 ? "0" + toHexString : toHexString);
+            }
+            return hexValue.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    // 结果测试：
+    public static void main(String[] args) throws Exception {
+        System.out.println(SHAEncrypt("123456"));
+    }
+}
