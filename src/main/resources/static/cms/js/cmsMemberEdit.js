2bd1fcb04a06ea2ea228877a675c2c4cddb196d5
May <may.chen82612090@gmail.com>
2024-09-14 17:39:04 +0800

    update cms member js / css / html

--- src/main/resources/static/cms/js/cmsMemberEdit.js
+++ src/main/resources/static/cms/js/cmsMemberEdit.js
@@ -12,12 +12,37 @@ const CmsMemberCreateDateTxt = document.querySelector("#CmsMemberCreateDate");
 const cmsMemberMoney = document.querySelector("#cmsMemberMoney");
 const cmsMemberImg = document.querySelector("#cmsMemberImg");
 const cmsMemberRemarkTxt = document.querySelector("#cmsMemberRemark");
+const cmsAddrList = document.querySelector("#cmsAddrList");
+
 
 // button setting
 const cancelBtn = document.querySelector("#cmsMemberCancel");
 const saveBtn = document.querySelector("#cmsMemberSave");
-document.addEventListener("DOMContentLoaded",()=>{
+const addrCollapseBtn = document.querySelector("#cmsMemberAddrCollapse");
 
+
+
+function getCmsList(){
+    fetch(`cms/manage`
+    )
+        .then(resp => resp.json())
+        .then(body => {
+            const {successful, message} = body;
+            if (successful) {
+                const {id, nickname, roleId, image} = body;
+                sessionStorage.setItem('id', id);
+                sessionStorage.setItem('nickname', nickname);
+                sessionStorage.setItem('roleId', roleId);
+                sessionStorage.setItem('image', image);
+                location = '../index.html';
+            } else {
+                errMsg.textContent = message;
+            }
+        });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    getCmsList();
 })
 cancelBtn.addEventListener("click", function () {
     Swal.fire({
@@ -30,7 +55,20 @@ cancelBtn.addEventListener("click", function () {
         cancelButtonText: "否"
     }).then((result) => {
         if (result.isConfirmed) {
-            location.href="./memberManage.html"
+            location.href = "./memberManage.html"
         }
     });
 });
+
+addrCollapseBtn.addEventListener("click", function () {
+    const collapseIcon = this.firstElementChild;
+
+    if(collapseIcon.classList.contains("fa-plus")){
+        console.log(1)
+    }
+    collapseIcon.classList.toggle("fa-plus");
+    collapseIcon.classList.toggle("fa-minus");
+    cmsAddrList.classList.toggle("hidden");
+
+
+})
