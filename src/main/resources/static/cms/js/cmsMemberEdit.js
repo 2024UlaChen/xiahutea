61385676c3eb662fe61baa488bd7fced3190a70f
May <may.chen82612090@gmail.com>
2024-09-15 21:59:20 +0800

    update cms html / css / js

--- src/main/resources/static/cms/js/cmsMemberEdit.js
+++ src/main/resources/static/cms/js/cmsMemberEdit.js
@@ -1,16 +1,15 @@
 //table all element
 const cmsMemberNameTxt = document.querySelector("#cmsMemberName");
-const cmsMemberSex = document.querySelector("#cmsSexList");
+const cmsMemberSex = document.querySelector("#cmsSex");
 const cmsMemberBirthdayTxt = document.querySelector("#cmsMemberBirthday");
 const cmsMemberEmailTxt = document.querySelector("#cmsMemberEmail");
 const cmsMemberPhoneTxt = document.querySelector("#cmsMemberPhone");
-const cmsMemberCity = document.querySelector("#cmsMemberCity");
+const cmsMemberCity = document.querySelector("#cmsMemberAddress");
 const cmsMemberDistrict = document.querySelector("#cmsMemberDistrict");
 const cmsMemberCarrierTxt = document.querySelector("#cmsMemberCarrier");
 const cmsMemberIsValidTxt = document.querySelector("#cmsMemberIsValid");
 const CmsMemberCreateDateTxt = document.querySelector("#CmsMemberCreateDate");
 const cmsMemberMoney = document.querySelector("#cmsMemberMoney");
-const cmsMemberImg = document.querySelector("#cmsMemberImg");
 const cmsMemberRemarkTxt = document.querySelector("#cmsMemberRemark");
 const cmsAddrList = document.querySelector("#cmsAddrList");
 
@@ -22,6 +21,26 @@ const addrCollapseBtn = document.querySelector("#cmsMemberAddrCollapse");
 
 
 document.addEventListener("DOMContentLoaded", () => {
+    let sessionDetail = JSON.parse(sessionStorage.getItem("memberDetail"));
+    cmsMemberBirthdayTxt.value = sessionDetail.birthday.replaceAll("/", "-");
+    CmsMemberCreateDateTxt.value = sessionDetail.createDate.replaceAll("/", "-");
+    cmsMemberCarrierTxt.value = sessionDetail.customerCarrier;
+    cmsMemberEmailTxt.value = sessionDetail.customerEmail;
+    // CmsMemberCreateDateTxt.value = sessionDetail.customerId;
+    cmsMemberMoney.value = sessionDetail.customerMoney;
+    cmsMemberPhoneTxt.value = sessionDetail.customerPhone;
+    cmsMemberRemarkTxt.value = sessionDetail.customerRemark;
+    cmsMemberNameTxt.value = sessionDetail.nickname;
+    cmsMemberIsValidTxt.checked = sessionDetail.validStatus;
+    if (sessionDetail.sex === "female") {
+        cmsMemberSex.value = "女性";
+    } else if (sessionDetail.sex === "male") {
+        cmsMemberSex.value = "男性";
+    } else {
+        cmsMemberSex.value = "不提供";
+    }
+
+    console.log(sessionDetail);
 })
 cancelBtn.addEventListener("click", function () {
     Swal.fire({
@@ -42,7 +61,7 @@ cancelBtn.addEventListener("click", function () {
 addrCollapseBtn.addEventListener("click", function () {
     const collapseIcon = this.firstElementChild;
 
-    if(collapseIcon.classList.contains("fa-plus")){
+    if (collapseIcon.classList.contains("fa-plus")) {
 
     }
     collapseIcon.classList.toggle("fa-plus");
@@ -50,5 +69,7 @@ addrCollapseBtn.addEventListener("click", function () {
     cmsAddrList.classList.toggle("hidden");
 
 
-
+})
+cmsMemberIsValidTxt.addEventListener("click", function (e) {
+    console.log(e.target);
 })
