bd483a6b993bb21ddaa60ec59db724afb089cff5
Ray <a55663613@gmail.com>
2024-09-17 14:14:09 +0800

    Revert "Merge branch 'RAY'"
    This reverts commit bc7ddad0a14bec6bb3014d878818a2c0437949c4, reversing
    changes made to 41feb0a605ab5b2791282af6a61d1d5e0cd23120.

--- src/main/resources/static/js/cart_detail.js
+++ src/main/resources/static/js/cart_detail.js
@@ -942,9 +942,7 @@ document.addEventListener("DOMContentLoaded",function(){
                 .catch(error => console.error('Error loading coupons:', error));
         }
         //F20 計算訂單金額
-        function Calculatetotal(){
 
-        }
 
 
     // 獲取尺寸的輔助函數
