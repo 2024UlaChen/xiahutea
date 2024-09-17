d377c6f1a0a82395b28c7f027fe5ee6e43cb8c42
Harper <wendy8564@gmail.com>
2024-09-17 18:11:59 +0800

    申請成功改用 sweetAlert 顯示

--- src/main/resources/static/js/joinUs.js
+++ src/main/resources/static/js/joinUs.js
@@ -28,9 +28,10 @@ $(form).submit(e => {
         body: formData
     }).then(res => res.json()).then(store => {
         if (store.successful) {
-            $(resultMessageEle).text("已收到加入申請，請耐心等待回復");
+            swal.fire("成功!", "已收到加入申請，請耐心等待回復", "success");
             $(".form-control").each((index, item) => {
                 $(item).val("");
+                $(resultMessageEle).text("")
             })
         } else {
             $(resultMessageEle).text(store.message)
