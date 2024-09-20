9c173cfd2ebb946e3d55c421b8322d0a14a64106
Harper <wendy8564@gmail.com>
2024-09-20 12:54:27 +0800

    店家申請成功後，加入totalUser

--- src/main/java/idv/tia201/g2/web/store/service/RegisterStoreService.java
+++ src/main/java/idv/tia201/g2/web/store/service/RegisterStoreService.java
@@ -1,6 +1,5 @@
 package idv.tia201.g2.web.store.service;
 
-import idv.tia201.g2.core.pojo.Core;
 import idv.tia201.g2.web.store.dto.RegisterStoreDTO;
 import idv.tia201.g2.web.store.vo.Store;
 import jakarta.mail.MessagingException;
