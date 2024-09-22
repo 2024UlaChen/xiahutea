cc9b40cf15384963547fa62ed0fc4aac02f80842
May <may.chen82612090@gmail.com>
2024-09-22 14:54:55 +0800

    add validatorUtil

--- src/main/java/idv/tia201/g2/web/member/controller/MemberController.java
+++ src/main/java/idv/tia201/g2/web/member/controller/MemberController.java
@@ -1,9 +1,8 @@
 package idv.tia201.g2.web.member.controller;
 
-
 import idv.tia201.g2.core.pojo.Core;
+import idv.tia201.g2.core.util.ValidateUtil;
 import idv.tia201.g2.web.member.service.MemberService;
-import idv.tia201.g2.web.member.util.CarrierUtil;
 import idv.tia201.g2.web.member.vo.Member;
 import idv.tia201.g2.web.member.vo.MemberAddress;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +16,7 @@ import java.util.List;
 public class MemberController {
     @Autowired
     private MemberService memberService;
+
 
     @GetMapping("address/{memberId}")
     public List<MemberAddress> getMemberAddress(@PathVariable Integer memberId) {
@@ -35,7 +35,7 @@ public class MemberController {
         String memberCarrier = member.getCustomerCarrier();
 
         if (type.equals("update")) {
-            if (StringUtils.isEmpty(memberCarrier) || !CarrierUtil.checkCarrier(memberCarrier)) {
+            if (!StringUtils.hasText(memberCarrier) || !ValidateUtil.checkCarrier(memberCarrier)) {
                 core.setMessage("carrier is wrong, plz check ");
                 core.setSuccessful(false);
                 return core;
