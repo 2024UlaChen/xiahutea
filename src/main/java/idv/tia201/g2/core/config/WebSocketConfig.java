d7e95b773f972f1ebd888db53ef136da394e4c95
Harper <wendy8564@gmail.com>
2024-09-22 16:58:43 +0800

    更新聊天室配置

--- src/main/java/idv/tia201/g2/core/config/WebSocketConfig.java
+++ src/main/java/idv/tia201/g2/core/config/WebSocketConfig.java
@@ -1,28 +1,25 @@
 package idv.tia201.g2.core.config;
 
+import idv.tia201.g2.web.chat.websocket.ChatWebSocketHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.*;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket-endpoint") // ←註冊端⼝
-                .addInterceptors(new HttpSessionHandshakeInterceptor())
-                .withSockJS();
-    }
-    // ←啟⽤SockJS⽀援
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/msg") // ←訊息控制器的網址前綴字
-                .enableSimpleBroker("/chat/chat");
+    @Bean
+    public WebSocketHandler webSocketHandler() {
+        return new ChatWebSocketHandler();
     }
 
-
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler(), "/chat")
+                .addInterceptors(new HttpSessionHandshakeInterceptor());  // 使用內建的攔截器
+    }
 }
