e3648fbee160bcd56e314ce81995691c326fc4b1
Harper <wendy8564@gmail.com>
2024-09-27 14:27:18 +0800

    更新取的 HttpSession 的方式

--- src/main/java/idv/tia201/g2/core/config/WebSocketConfig.java
+++ src/main/java/idv/tia201/g2/core/config/WebSocketConfig.java
@@ -1,11 +1,13 @@
 package idv.tia201.g2.core.config;
 
+import idv.tia201.g2.core.filter.HttpSessionInterceptor;
 import idv.tia201.g2.web.chat.websocket.ChatWebSocketHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
@@ -19,7 +21,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler(), "/chat")
-                .addInterceptors(new HttpSessionHandshakeInterceptor());  // 使用內建的攔截器
+        registry.addHandler(webSocketHandler(), "cms/chat")
+                .addInterceptors(new HttpSessionInterceptor());  // 加入攔截器
     }
 }
