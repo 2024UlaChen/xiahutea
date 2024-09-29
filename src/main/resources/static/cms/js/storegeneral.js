32f545c8d4fe8d9a9716e4f352a6b05a5d9599b4
Harper <wendy8564@gmail.com>
2024-09-29 23:10:28 +0800

    依登入的 userTypeId 決定側邊欄顯示

--- src/main/resources/static/cms/js/storegeneral.js
+++ src/main/resources/static/cms/js/storegeneral.js
@@ -16,3 +16,28 @@ function myLoad(target,url){
         }
     });
 }
+
+$(function (){
+    console.log("側邊欄更新")
+    let showStore = $("li.nav-item.show-store")
+    let showAdmin = $("li.nav-item.show-admin")
+    fetch("/totalusers")
+        .then(res => res.json())
+        .then(data => {
+            userTypeId = data.userTypeId;
+            console.log(userTypeId);
+            if(userTypeId === 3){
+                $.each(showStore,function (index,item){
+                    $(item).addClass("d-none")
+                })
+                console.log("管理員顯示")
+            }else if (userTypeId === 1){
+                $.each(showAdmin,function (index,item){
+                    $(item).addClass("d-none")
+                })
+                console.log("店家顯示")
+            }else{
+                $("aside").addClass("d-none")
+            }
+        })
+})
