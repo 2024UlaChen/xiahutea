0b39810d034f5bb4b89cf51092a35fce29c51b85
May <may.chen82612090@gmail.com>
2024-09-29 12:37:46 +0800

    update address

--- src/main/resources/static/js/memberAddress.js
+++ src/main/resources/static/js/memberAddress.js
@@ -1,5 +1,17 @@
+// console.log(window.DEFAULT_ADDRESS.data);
+const citySet = new Set();
+const addrSet = new Set();
+const defaultCity = "台北市";
+$.each(window.DEFAULT_ADDRESS.data, function (index, item) {
+    citySet.add(item.city);
+    addrSet.add(item.city + "-" + item.district);
+})
+
+
 const memberAddressUl = document.querySelector("#memberAddress");
-const addressSavingBtn = document.querySelector(".addressSaving");
+const addrModalBody = document.querySelector("#addrModalBody");
+const newAddrBtn = document.querySelector(".newAddr");
+const saveAddrBtn = document.querySelector(".saveAddr");
 
 const sessionDetail = JSON.parse(sessionStorage.getItem("memberData"));
 
@@ -16,58 +28,80 @@ function getAddress(memberId) {
             $.each(data.data, function (index, item) {
                 let addrNum = item.customerAddressId;
                 addrList += `
-                            <li data-addressID="${addrNum}">
-                                <i class="fa fa-map-marker" aria-hidden="true"></i>
-                                <span>${item.customerAddress}</span>
-                                <button class="editAddr" data-address-edit="${addrNum}">
-                                    <i class="fa fa-pencil editAddr"></i>
-                                </button>
-                                <button class="delAddr" data-address-delete="${addrNum}">
-                                    <i class="fa fa-trash delAddr"></i>
-                                </button>
-                            </li>
+                    <li data-addressId="${addrNum}">
+                        <i class="fa fa-map-marker"></i>
+                        <span>${item.customerAddress}</span>
+                        <button class="editAddr" data-address-edit="${addrNum}" data-target="#addrModal" data-toggle="modal">
+                            <i class="fa fa-pencil editAddr"></i>
+                        </button>
+                        <button class="delAddr" data-address-delete="${addrNum}">
+                            <i class="fa fa-trash delAddr"></i>
+                        </button>
+                    </li>
                 `;
             });
             addrList += `<hr>`
-            // console.log(addrList);
             memberAddressUl.innerHTML = addrList;
             // cmsAddrList.classList.add("hidden");
         })
 }
 
-async function editAddr() {
-    const {value: fruit} = await Swal.fire({
-        title: "Select field validation",
-        input: "select",
-        inputOptions: {
-            Fruits: {
-                apples: "Apples",
-                bananas: "Bananas",
-                grapes: "Grapes",
-                oranges: "Oranges"
-            },
-            Vegetables: {
-                potato: "Potato",
-                broccoli: "Broccoli",
-                carrot: "Carrot"
-            },
-            icecream: "Ice cream"
-        },
-        inputPlaceholder: "Select a fruit",
-        showCancelButton: true,
-        inputValidator: (value) => {
-            return new Promise((resolve) => {
-                if (value === "oranges") {
-                    resolve();
-                } else {
-                    resolve("You need to select oranges :)");
-                }
-            });
+
+function getDefaultCity(nowCity) {
+    let cityList = "";
+    let otherCityList = "";
+    citySet.forEach((item) => {
+        if (item === nowCity) {
+            cityList += `<option value="${item}">${item}</option>`
+        } else {
+            otherCityList += `<option value="${item}">${item}</option>`
         }
-    });
-    if (fruit) {
-        Swal.fire(`You selected: ${fruit}`);
-    }
+    })
+    cityList += otherCityList;
+    return cityList;
+}
+
+function getDefaultDistrict(city) {
+    let districtList = "";
+    addrSet.forEach((item) => {
+        if (item.includes(city)) {
+            let modalDistrictList = item.split("-")[1];
+            districtList += `<option value="${modalDistrictList}">${modalDistrictList}</option>`
+        }
+    })
+    return districtList;
+}
+
+// 編輯現有地址
+function editAddr(target) {
+    let dataAddressId = target.closest("li").getAttribute("data-addressId");
+    let nowAddr = target.closest("li").querySelector("span").innerText;
+    let addrList = nowAddr.split("-");
+    let modalBodyData = "";
+    modalBodyData += `
+            <span class="form-label"><span class="warn">*</span> 縣市區域 </span>
+            <div>
+                <div class="member-address col-sm-12 no-padding">
+                    <select class="form-select col-lg-6 modalCity"
+                            aria-label="Default select example" id="modalCity">
+                        ${getDefaultCity(addrList[0])}
+                    </select>
+                    <select class="form-control-wrap form-select col-lg-6 modalDistrict"
+                            aria-label="Default select example" id="modalDistrict">
+                        <option selected>${addrList[1]}</option>
+                        ${getDefaultDistrict(addrList[0])}
+                    </select>
+                </div>
+            </div>
+            <span class="form-label"><span class="warn">*</span> 詳細地址 </span>
+            <div>
+                <input title="detail address" type="text" name="your detail address" 
+                size="100" id="nowDetailAddr" class="form-control" value="${addrList[2]}">
+            <div>
+    `;
+    addrModalBody.innerHTML = modalBodyData;
+    addrModalBody.setAttribute("data-addressId", dataAddressId);
+
 }
 
 
@@ -80,21 +114,70 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 document.addEventListener("click", function (e) {
-    let parentBtn = e.target.closest("button");
-
-    if (e.target.classList.contains("editAddr") ) {
-        // data-address-edit="${addrNum}
-        editAddr();
-    } else if (e.target.classList.contains("delAddr") ) {
-        console.log(2);
-
-        e.target.closest("tr").querySelectorAll("td").forEach(item => {
-            if (item.dataset.memberid !== undefined) {
-                getCmsMemberInfoById(item.dataset.memberid);
-            }
-        })
-    }else{
-        e.stopPropagation();
+    if (e.target.classList.contains("editAddr")) {
+        editAddr(e.target);
+    } else if (e.target.classList.contains("delAddr")) {
+    } else {
+        // e.stopPropagation();
     }
+})
 
+document.addEventListener("change", function (e) {
+    let changeDistrict = document.querySelector("#modalDistrict");
+    let changeDetailAddr = document.querySelector("#nowDetailAddr");
+
+    if (e.target.classList.contains("modalCity")) {
+        changeDetailAddr.value = "";
+        changeDistrict.innerHTML = getDefaultDistrict(e.target.value);
+    }
+    if (e.target.classList.contains("modalDistrict")) {
+        changeDetailAddr.value = "";
+    }
+})
+
+// 新增地址
+newAddrBtn.addEventListener("click", function () {
+    let modalBodyData = "";
+    modalBodyData += `
+            <span class="form-label"><span class="warn">*</span> 縣市區域 </span>
+            <div>
+                <div class="member-address col-sm-12 no-padding">
+                    <select class="form-select col-lg-6 modalCity"
+                            aria-label="Default select example" id="modalCity">
+                        ${getDefaultCity(defaultCity)}
+                    </select>
+                    <select class="form-control-wrap form-select col-lg-6 modalDistrict"
+                            aria-label="Default select example" id="modalDistrict">
+                            ${getDefaultDistrict(defaultCity)}
+                    </select>
+                </div>
+            </div>
+            <span class="form-label"><span class="warn">*</span> 詳細地址 </span>
+            <div>
+                <input title="detail address" type="text" name="your detail address"
+                size="100" id="nowDetailAddr" class="form-control" >
+            <div>
+    `;
+    addrModalBody.innerHTML = modalBodyData;
+    addrModalBody.setAttribute("data-addressId", "default");
+})
+
+saveAddrBtn.addEventListener("click", function (e) {
+    let modalBody = e.target.closest(".modal-content").querySelector("#addrModalBody");
+    let saveCity = modalBody.querySelector("#modalCity").value;
+    let saveDistrict = modalBody.querySelector("#modalDistrict").value;
+    let saveDetailAddr = modalBody.querySelector("#nowDetailAddr").value;
+    let finalAddr = saveCity + "-" + saveDistrict + "-" + saveDetailAddr;
+    if (saveDetailAddr === "") {
+        Swal.fire("詳細地址不可為空", "", "error");
+    } else if (!saveDetailAddr.includes("路")) {
+        Swal.fire("詳細地址必須包含路名", "", "error");
+    } else {
+        let sendDataAddressId = modalBody.getAttribute("data-addressId");
+        if (sendDataAddressId === "default") {
+            console.log(sendDataAddressId);
+        } else {
+            console.log(sendDataAddressId);
+        }
+    }
 })
