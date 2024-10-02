9054c37b28401733a4019aaa1bde6a0b66fb30d8
Kun <s58803194@gmail.com>
2024-10-02 16:23:31 +0800

    調整首頁的css命名 加入用戶的集點卡API

--- src/main/java/idv/tia201/g2/web/member/dao/MemberLoyaltyCardRepository.java
+++ src/main/java/idv/tia201/g2/web/member/dao/MemberLoyaltyCardRepository.java
@@ -3,9 +3,12 @@ package idv.tia201.g2.web.member.dao;
 import idv.tia201.g2.web.store.vo.CustomerLoyaltyCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberLoyaltyCardRepository extends JpaRepository<CustomerLoyaltyCard,Integer> {
     CustomerLoyaltyCard findByStoreIdAndMemberId(Integer storeId, Integer memberId);
     CustomerLoyaltyCard findByLoyaltyCardId(Integer loyaltyCardId);
+    List<CustomerLoyaltyCard> findCustomerLoyaltyCardsByMemberId(Integer customerId);
 
 
 
--- src/main/java/idv/tia201/g2/web/member/service/MemberLoyaltyCardService.java
+++ src/main/java/idv/tia201/g2/web/member/service/MemberLoyaltyCardService.java
@@ -2,10 +2,14 @@ package idv.tia201.g2.web.member.service;
 
 import idv.tia201.g2.web.store.vo.CustomerLoyaltyCard;
 
+import java.util.List;
+
 public interface MemberLoyaltyCardService {
     CustomerLoyaltyCard UpdatePoints(Integer loyaltyCardId, Integer decreasePoints);
 
     CustomerLoyaltyCard GetCustomerLoyaltyCard(Integer loyaltyCardId);
+
+    List<CustomerLoyaltyCard> GetCustomerLoyaltyCards(Integer memberId);
 
     boolean AddCustomerLoyaltyCard(CustomerLoyaltyCard customerLoyaltyCard);
 }
