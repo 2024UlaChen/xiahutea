3bf9310f3042f8d95a2cb62f436e930c2cf0722b
Harper <wendy8564@gmail.com>
2024-10-02 20:56:01 +0800

    管理員登入成功之後，加入 cookie

--- src/main/java/idv/tia201/g2/web/user/controller/AdminController.java
+++ src/main/java/idv/tia201/g2/web/user/controller/AdminController.java
@@ -1,10 +1,12 @@
 package idv.tia201.g2.web.user.controller;
 
+import idv.tia201.g2.core.util.EncrypSHA;
 import idv.tia201.g2.web.user.dto.TotalUserDTO;
 import idv.tia201.g2.web.user.service.AdminService;
 import idv.tia201.g2.web.user.vo.Administrator;
-import idv.tia201.g2.web.user.vo.TotalUsers;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,15 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/admin")
-public class
-AdminController {
+public class AdminController {
     private static final long serialVersionUID = 1L;
     @Autowired
     private AdminService adminService;
 
-//    @ResponseBody
     @PostMapping("/login")
-    public TotalUserDTO adminlogin(Administrator admin, HttpServletRequest request) {
+    public TotalUserDTO adminlogin(Administrator admin, HttpServletRequest request, HttpServletResponse response) {
 //        若無資料
         if (admin == null) {
             TotalUserDTO totalUserDTO = new TotalUserDTO();
@@ -31,6 +31,8 @@ AdminController {
         }
 
         TotalUserDTO totalUserDTO = adminService.login(admin);
+
+//        設置Session & Cookie
         if (totalUserDTO.isSuccessful()) {
             if (request.getSession(false) != null) {
                 request.changeSessionId();
@@ -38,7 +40,23 @@ AdminController {
             final HttpSession session = request.getSession();
             session.setAttribute("loggedin", true);
             session.setAttribute("totalUserDTO", totalUserDTO);
+
+            // 設置一個 Cookie
+            String SHAUserneme = EncrypSHA.SHAEncrypt(admin.getAdminUsername());
+            String SHAPassword = EncrypSHA.SHAEncrypt(admin.getAdminPassword());
+            Cookie usernameCookie = new Cookie("username", SHAUserneme);
+            Cookie passwordCookie = new Cookie("password", SHAPassword);
+            // 設置過期時間（秒）
+            usernameCookie.setMaxAge(60 * 60 * 24 * 7); // 1周
+            passwordCookie.setMaxAge(60 * 60 * 24 * 7); // 1周
+            // 設置路徑
+            usernameCookie.setPath("/");
+            passwordCookie.setPath("/");
+            // 發送給 client 端
+            response.addCookie(usernameCookie);
+            response.addCookie(passwordCookie);
         }
+
         return totalUserDTO;
     }
 
