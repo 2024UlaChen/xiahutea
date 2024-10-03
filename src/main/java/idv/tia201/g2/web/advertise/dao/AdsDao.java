f71d867afe7c40a65fd2ff5ccba6de6b70b6d5fe
Kun <s58803194@gmail.com>
2024-10-03 23:03:51 +0800

    新增前台取得商家資訊，取得首頁輪播廣告

--- src/main/java/idv/tia201/g2/web/advertise/dao/AdsDao.java
+++ src/main/java/idv/tia201/g2/web/advertise/dao/AdsDao.java
@@ -3,9 +3,11 @@ package idv.tia201.g2.web.advertise.dao;
 import idv.tia201.g2.web.advertise.vo.Advertise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 public interface AdsDao extends JpaRepository<Advertise,Integer> {
     public Advertise findBytitle(String title);
     public List<Advertise> findByAdsTotalUserid(Long adsTotalUserid);
+    List<Advertise> findByHomeDisplayInAndIsactiveInAndStartTimeBetween(boolean homeDisplayIn, boolean isActiveIn, Timestamp startTime, Timestamp endTime );
 }
