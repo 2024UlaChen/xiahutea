dd737cd90f947d55286782e0c92d1eb1054d6914
May <may.chen82612090@gmail.com>
2024-10-05 09:03:53 +0800

    update login

--- src/main/resources/static/js/loginOrRegister.js
+++ src/main/resources/static/js/loginOrRegister.js
@@ -184,7 +184,7 @@ btnLogin.addEventListener("click", function () {
                     timer: 1500
                 }).then(() => {
                     sessionStorage.setItem("memberData", JSON.stringify(data));
-                    location.replace("../login.html");
+                    location.replace("../homePage.html");
                 })
             } else {
                 Swal.fire(data.message, "", "error");
