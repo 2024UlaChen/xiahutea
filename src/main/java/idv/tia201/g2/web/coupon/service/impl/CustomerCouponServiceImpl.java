575f2740a3b5c5faebc0475e495e7355833cd3c7
Kun <s58803194@gmail.com>
2024-10-06 23:57:43 +0800

    發送優惠券給會員

--- src/main/java/idv/tia201/g2/web/coupon/service/impl/CustomerCouponServiceImpl.java
+++ src/main/java/idv/tia201/g2/web/coupon/service/impl/CustomerCouponServiceImpl.java
@@ -3,16 +3,22 @@ package idv.tia201.g2.web.coupon.service.impl;
 import idv.tia201.g2.web.coupon.dao.CustomerCouponDao;
 import idv.tia201.g2.web.coupon.service.CustomerCouponService;
 import idv.tia201.g2.web.coupon.vo.CustomerCoupons;
+import idv.tia201.g2.web.member.dao.MemberDao;
+import idv.tia201.g2.web.member.vo.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CustomerCouponServiceImpl implements CustomerCouponService {
     @Autowired
     private CustomerCouponDao customerCouponDao;
+
+    @Autowired
+    private MemberDao memberDao;
 
     @Override
     public void updateCouponQuantity(Integer customerId, Integer couponId, Integer newQuantity) {
@@ -50,6 +56,24 @@ public class CustomerCouponServiceImpl implements CustomerCouponService {
         ForResgiter.setCouponId(1);
         ForResgiter.setCouponQuantity(1);
         return customerCouponDao.save(ForResgiter);
+    }
+
+    @Override
+    public void SendCouponsToMembers(Integer couponId) {
+        //取得目前的會員
+        List<Member> memberList = memberDao.findAllMember();
+
+        List<CustomerCoupons> list = new ArrayList<CustomerCoupons>();
+
+
+        for (Member member : memberList) {
+            CustomerCoupons customerCoupons = new CustomerCoupons();
+            customerCoupons.setCouponId(couponId);
+            customerCoupons.setCouponQuantity(1);
+            customerCoupons.setCustomerId(member.getCustomerId());
+            list.add(customerCoupons);
+        }
+        customerCouponDao.saveAll(list);
     }
 
 }
